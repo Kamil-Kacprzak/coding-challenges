546fd0afabf2f9e0138452906406a30a2eebf08e
Kamil-Kacprzak <Kamil.Kacprzak@yahoo.com>
2021-10-12 02:00:52 +0200

    Birthday cake candles - Collections max and frequency usage

---
+++ src/hackerrank/problemsolving/algorithms/BdayCakeCandles.java
@@ -0,0 +1,44 @@
+package hackerrank.problemsolving.algorithms;
+
+/*
+ * https://www.hackerrank.com/challenges/birthday-cake-candles/problem?h_r=next-challenge&h_v=zen
+ */
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+
+public class BdayCakeCandles {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int candlesCount = Integer.parseInt(bufferedReader.readLine().trim());
+
+        List<Integer> candles = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+            .map(Integer::parseInt)
+            .collect(toList());
+
+        int result = birthdayCakeCandles(candles);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+    
+    public static int birthdayCakeCandles(List<Integer> candles) {
+    	int max = Collections.max(candles);    	
+    	return Collections.frequency(candles, max);
+    }
+}
