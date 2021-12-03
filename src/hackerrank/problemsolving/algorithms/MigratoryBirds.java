a4e562358ae0a0034c1ec147e91c2fec3f953cb7
Kamil-Kacprzak <Kamil.Kacprzak@yahoo.com>
2021-12-03 23:18:42 +0100

    Migratory Birds- Frequency of occurrence in collections

---
+++ src/hackerrank/problemsolving/algorithms/MigratoryBirds.java
@@ -0,0 +1,52 @@
+package hackerrank.problemsolving.algorithms;
+/*
+ * https://www.hackerrank.com/challenges/migratory-birds/problem?isFullScreen=true
+ */
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+public class MigratoryBirds {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int arrCount = Integer.parseInt(bufferedReader.readLine().trim());
+
+        List<Integer> arr = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+            .map(Integer::parseInt)
+            .collect(toList());
+
+        int result = migratoryBirds(arr);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+    
+    public static int migratoryBirds(List<Integer> arr) {
+    	int result = 0;
+    	int resultId = 0;
+    	HashSet<Integer> singular = new HashSet<Integer>(arr);
+    	int tmp;
+    	for (int i : singular) {
+    		tmp = Collections.frequency(arr, i);
+    		if(tmp > result || (tmp == result && i < resultId)) {
+    			result = tmp;
+    			resultId = i;
+    		}
+    	}
+    	return resultId;
+    }
+}
